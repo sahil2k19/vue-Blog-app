9e79efc4f808bfa966748332d3f959dfa77fb14f
sahil2k19 <sahilgagan227@gmail.com>
2023-03-01 12:14:34 +0530

    first commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,56 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="home">
+      Hello
+      <p ref="p">MY name is {{ person.name }} and my age is {{ person.age }}</p>
+      <button @click="person.age++">Inc AGE</button>
+      <button @click="handleClick" >Click me</button>
+      <input type="text"  v-model="person.name">
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import {ref,reactive} from 'vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  setup(){
+    console.log("setup");
+
+    const p = ref(null);
+
+    const person = reactive({name:"ravi", age:"29"})
+    // let name  = ref("mario");
+    // let age = ref(30);
+    // let name  = "sahil";
+    // let age = 49;
+
+    const handleClick=()=>{
+      person.name = 'sahil';
+      person.age = '20';
+      // name = "ravi"
+      // age  = 90
+
+    }
+    
+
+    return {person, handleClick, p }
+  },
+
+  data(){
+    return{
+    score:5,
+    }
+  },
+  created(){
+    console.log("created")
+    
+  },
+  mounted(){
+    console.log("mounted") 
+  },
 }
 </script>
